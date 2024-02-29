9f22de1af059b051acfe6e366c29e84d43eaf7dd
nxwbtk <buntakan0703@gmail.com>
2024-02-29 17:34:31 +0700

    Add age.py and whatsyourname.py scripts

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+
+first_name = input("Hey, what's your first name? : ").split()
+last_name = input("And your last name? : ").split()
+
+print(f'Well, pleased to meet you, {first_name[0]} {last_name[0]}!')
